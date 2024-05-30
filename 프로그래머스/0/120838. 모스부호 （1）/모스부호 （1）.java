e68a28f523d0be745914d88857c81d72cbd12242
JangHyosun <137710845+HAILEYHS@users.noreply.github.com>
2024-05-30 20:19:17 +0900

    [level 0] Title: 모스부호 (1), Time: 10.22 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        String [] morseList = {".-","-...","-.-.","-..",".","..-.","--.","....",
+                               "..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+                               ".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String [] morse;
+        morse = letter.split(" ");
+        for(int i=0; i<morse.length; i++){
+            for(int j=0; j<morseList.length; j++){
+                if(morse[i].equals(morseList[j])){
+                    answer += (char)(j+97);
+                }
+            }
+        }
+        return answer;
+    }
+}
